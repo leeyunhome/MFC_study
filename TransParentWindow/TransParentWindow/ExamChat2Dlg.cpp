075171817499d0616ca6c969171592af55652f03
leeyunhome <leeyunhome@daum.net>
2020-04-26 15:19:38 +0900

    add 윈도우 특정 부분 투명화

---
+++ TransParentWindow/TransParentWindow/ExamChat2Dlg.cpp
@@ -0,0 +1,99 @@
+
+// ExamChat2Dlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ExamChat2.h"
+#include "ExamChat2Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CExamChat2Dlg dialog
+
+
+
+CExamChat2Dlg::CExamChat2Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_EXAMCHAT2_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CExamChat2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CExamChat2Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+// CExamChat2Dlg message handlers
+
+BOOL CExamChat2Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	SetLayeredWindowAttributes(RGB(255, 1, 7), 0, LWA_COLORKEY);
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CExamChat2Dlg::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+
+	if (IsIconic())
+	{
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		dc.FillSolidRect(10, 10, 200, 200, RGB(255, 1, 7));
+		//CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CExamChat2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CExamChat2Dlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+	dc.FillSolidRect(point.x - 10, point.y - 10, 20, 20, RGB(255, 1, 7));
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
