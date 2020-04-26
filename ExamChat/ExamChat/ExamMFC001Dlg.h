0cdcabd9e0ec741ff25ae0d7fd8ca14b53f0fcca
leeyunhome <leeyunhome@daum.net>
2020-04-26 14:11:20 +0900

    add listbox

---
+++ ExamChat/ExamChat/ExamMFC001Dlg.h
@@ -0,0 +1,36 @@
+
+// ExamMFC001Dlg.h : header file
+//
+
+#pragma once
+
+
+// CExamMFC001Dlg dialog
+class CExamMFC001Dlg : public CDialogEx
+{
+// Construction
+public:
+	CExamMFC001Dlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXAMMFC001_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_chat_list;
+	afx_msg void OnBnClickedAddBtn();
+};
